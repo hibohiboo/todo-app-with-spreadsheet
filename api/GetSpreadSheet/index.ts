0602ecc0580811a25c15dc96a851898b48d770f7
hibohiboo <swdice66@gmail.com>
2020-12-07 08:24:04 +0900

    test #5 AB#19

--- api/GetSpreadSheet/index.ts
+++ api/GetSpreadSheet/index.ts
@@ -1,23 +1,29 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 import { execAPI } from '../google'
+import path from 'path'
+import { readFile } from 'fs/promises'
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest,
 ): Promise<void> {
   try {
     const list = await execAPI(
-      process.env.SPREAD_SHEET_ID,
-      process.env.SPREAD_SHEET_RANGE,
+      process.env.APPSETTING_SPREAD_SHEET_ID,
+      process.env.APPSETTING_SPREAD_SHEET_RANGE,
     )
 
     context.res = {
       body: list,
     }
   } catch (e) {
-    context.res = {
-      status: 503,
-      body: { e, env: process.env, dirName: __dirname },
-    }
+    // context.res = {
+    //   status: 503,
+    //   body: { e, env: process.env, dirName: __dirname },
+    // }
+    const file = await readFile(
+      path.join(__dirname, '../google/serviceAccountKey.json'),
+    )
+    context.res = { body: { message: 'server error', file } }
   }
 }
 
