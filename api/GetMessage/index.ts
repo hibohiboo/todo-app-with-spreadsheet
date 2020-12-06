7d1b53f9e591c4350bbe382ef6b15f621e855451
hibohiboo <swdice66@gmail.com>
2020-12-06 19:56:45 +0900

    add linter #4 AB#18

--- api/GetMessage/index.ts
+++ api/GetMessage/index.ts
@@ -1,13 +1,15 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-        context.res = {
-          body: {
-            text: "Hello from the API"
-          }
-        };
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest,
+): Promise<void> {
+  context.log('HTTP trigger function processed a request.')
+  context.res = {
+    body: {
+      text: 'Hello from the API',
+    },
+  }
+}
 
-};
-
-export default httpTrigger;
+export default httpTrigger
