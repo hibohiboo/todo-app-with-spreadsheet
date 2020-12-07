6ace6bdb4e55d7e751f01b5e8599f0f194231053
hibohiboo <swdice66@gmail.com>
2020-12-07 19:26:41 +0900

    fix env replace #5 AB#19

--- api/google/index.ts
+++ api/google/index.ts
@@ -3,11 +3,17 @@ import { google } from 'googleapis'
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 const sheets = google.sheets('v4')
 
+// functionsの環境変数から秘密鍵を読み込んだ時に、\が\\になってしまい鍵を認識しないので置換
+const private_key = process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(
+  '\\\\',
+  '\\',
+)
+
 export async function execAPI(spreadsheetId, range) {
   const auth = await google.auth.getClient({
     credentials: {
       client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-      private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+      private_key,
     },
     scopes: SCOPES,
   })
