1193c2bb86fb393cb7cc52c037bf1e41e99d766c
hibohiboo <swdice66@gmail.com>
2020-12-08 00:25:15 +0900

    test fetch create #6 AB#21

--- api/google/index.ts
+++ api/google/index.ts
@@ -1,15 +1,17 @@
 import { google } from 'googleapis'
 import type { Todo, DataStorage } from '@/types'
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+import _ from 'lodash'
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 const sheets = google.sheets('v4')
-
+const spreadsheetId = process.env.APPSETTING_SPREAD_SHEET_ID
+const range = process.env.APPSETTING_SPREAD_SHEET_RANGE
 // functionsの環境変数から秘密鍵を読み込んだ時に、\が\\になってしまい鍵を認識しないので置換
 const private_key = process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(
   '\\\\',
   '\\',
 )
 
-export async function execAPI(spreadsheetId, range) {
+async function getAuth() {
   const auth = await google.auth.getClient({
     credentials: {
       client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
@@ -17,33 +19,67 @@ export async function execAPI(spreadsheetId, range) {
     },
     scopes: SCOPES,
   })
+  return auth
+}
 
+async function fetchAll(spreadsheetId: string, range: string) {
+  const auth = await getAuth()
   const apiOptions = {
     auth,
     spreadsheetId,
     range,
   }
-  return new Promise((resolve, reject) => {
+  return new Promise<string[][]>((resolve, reject) => {
     sheets.spreadsheets.values.get(apiOptions, (err, res) => {
       if (err) {
         reject(err)
         return
       }
+      console.log('fetch', res.data)
+      if (!res.data.values) return resolve([] as any)
       console.log(res.data.values)
       resolve(res.data.values)
     })
   })
 }
 
+export const execAPI = fetchAll
+
+const SheetArrayToToDo = (arr: string[]): Todo => ({
+  id: arr[0],
+  title: arr[1],
+  completed: arr[2] === 'TRUE',
+})
+
 const exportsObj: DataStorage<Todo> = {
   fetchAll: async () => {
-    return Promise.all([])
+    const list = await fetchAll(spreadsheetId, range)
+    return list.map(SheetArrayToToDo)
   },
   fetchByCompleted: (completed) => {
     return exportsObj.fetchAll()
   },
   create: async (todo) => {
-    console.log(todo)
+    const auth = await getAuth()
+    const values = _.values(todo)
+    // APIを呼び出して、行の追加処理
+    const req = {
+      auth,
+      // シートのID
+      spreadsheetId: spreadsheetId,
+      // A1に追記することを指定
+      range: 'A1',
+      // 追記する形式を指定。
+      valueInputOption: 'USER_ENTERED',
+      // A1に値があったら下方向に空欄を探しにいく
+      insertDataOption: 'INSERT_ROWS',
+      // 追加する行のデータ。2次元配列で指定
+      resource: {
+        values: [values],
+      },
+    }
+    await sheets.spreadsheets.values.append(req)
+    console.log('crated', todo)
   },
   update: async (id, update) => {
     return { id, title: '', completed: false }
